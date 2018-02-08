ed975ac349adecc9dbeef75b2d55da37f950f5e0
JacKuna <kunalsaha290@yahoo.com>
2018-02-09 01:10:35 +0530

    Update install_ruby_nag01.sh

--- install_ruby_nag01.sh
+++ install_ruby_nag01.sh
@@ -29,3 +29,39 @@ rvm install 2.5.0
 rvm use 2.5.0 --default
 ruby --version
 
+gem install dashing
+gem install bundle
+
+
+# Application Setup begins here.
+
+useradd dashingadmin
+usermod -p '0wPGGsC1N8yYk' dashingadmin
+
+
+cd /home/dashingadmin/
+
+wget http://xxx.xx.xx.xx/nagraj.tar.gz
+tar -xvf  nagraj.tar.gz
+
+cd nagraj
+rm -rf history.yml
+
+echo "Please enter your nagios url only, don't add /nagios in the end"
+echo "Example : http://mynagios.server.com "
+read -p 'Your Nagios Monitoring Server URL, include http/https://yournagiosURL  : ' url
+
+read -p 'Your Nagios Username  : ' usr
+read -p 'Your Nagios Username"s password  : ' pass
+
+
+sed  -i "s,https://nagios.example.com,$url,g" jobs/sample.rb
+sed  -i "s,nagiosadmin:redhat@123,$usr:$pass,g" jobs/sample.rb
+sed  -i "s,https://nagios.example.com,$url,g" jobs/hostup.rb
+sed  -i "s,https://nagios.example.com,$url,g" dashboards/nagios.erb
+sed  -i "s,https://nagios.example.com,$url,g" dashboards/sample.erb
+
+chown -R dashingadmin.dashingadmin /home/dashingadmin/nagraj/
+
+su dashingadmin -c 'bundle'
+su dashingadmin -c 'dashing start'
