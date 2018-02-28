4aebb2a84e70abc2ec8b9487aead8fb3260b619d
Kunal <kunalsaha290@yahoo.com>
2018-03-01 01:00:43 +0530

    Removed dashing configuration, standalone script to install ruby

--- ruby_installation.sh
+++ ruby_installation.sh
@@ -29,41 +29,7 @@ rvm use 2.5.0 --default
 ruby --version
 
 gem install dashing
-gem install bundle
+gem install bundler
 
 
-# Application Setup begins here.
 
-useradd dashingadmin
-usermod -p '0wPGGsC1N8yYk' dashingadmin
-
-
-cd /home/dashingadmin/
-
-wget https://github.com/Jackuna/Nagraj/archive/development.zip
-
-unzip  development.zip
-mv mv Nagraj-development nagraj
-
-
-cd nagraj
-rm -rf history.yml
-
-echo "Please enter your nagios url only, don't add /nagios in the end"
-echo "Example : http://mynagios.server.com "
-read -p 'Your Nagios Monitoring Server URL, include http/https://yournagiosURL  : ' url
-
-read -p 'Your Nagios Username  : ' usr
-read -p 'Your Nagios Username"s password  : ' pass
-
-
-sed  -i "s,https://nagios.example.com,$url,g" jobs/nagraj.rb
-sed  -i "s,nagiosadmin:123,$usr:$pass,g" jobs/nagraj.rb
-
-sed  -i "s,https://nagios.example.com,$url,g" dashboards/nagraj.erb
-sed  -i "s,https://nagios.example.com,$url,g" dashboards/nagrajtv.erb
-
-chown -R dashingadmin.dashingadmin /home/dashingadmin/nagraj/
-
-su dashingadmin -c 'bundle'
-su dashingadmin -c 'dashing start'
